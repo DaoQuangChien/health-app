35fe71297265fafffe17d17dce35bf8b022c5140
Chien Dao <chiendao@192.168.2.2>
2023-04-07 14:40:58 +0700

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#FFFFFF",
+        "primary-300": "#FFCC21",
+        "primary-400": "#FF963C",
+        "primary-500": "#EA6C00",
+        "secondary-300": "#8FE9D0",
+        "dark-600": "#2E2E2E",
+        "dark-500": "#414141",
+        "gray-400": "#777777",
+      },
+    },
+  },
+  plugins: [],
+};
